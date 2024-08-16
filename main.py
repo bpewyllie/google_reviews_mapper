128eac99933ce2112dd21e32c81953dadc849e33
bpewyllie <bpewyllie@utexas.edu>
2024-08-15 22:50:48 -0600

    update formatting

--- main.py
+++ main.py
@@ -16,8 +16,8 @@ dotenv.load_dotenv()
 API_KEY = os.getenv("GMAPS_API_KEY")
 LOCATION = "40.7606586, -111.8927191"
 RADIUS = 500
-ITERATIONS = 1
-OUTPUT_DIR = f"./output/data/{datetime.date.today().strftime('%Y-%m-%d')}"
+ITERATIONS = 10
+OUTPUT_DIR = f"./output/data/{datetime.datetime.now().strftime('%Y-%m-%d-%H-%M')}"
 
 
 if __name__ == "__main__":
@@ -29,23 +29,40 @@ if __name__ == "__main__":
 
     print("generating grid of locations...")
     locations = gps.tools.generate_grid_locs(LOCATION, RADIUS, ITERATIONS)
-    
-    print("filtering to SLC...")
-    county_gdf = geopandas.read_file("https://slco.org/slcogis/rest/services/Administration/MapServer/3/query?outFields=*&where=1%3D1&f=geojson").to_crs(epsg=4326)
-    city_gdf = county_gdf.loc[county_gdf["NAME"] == "SALT LAKE"]
+    print(f"\n{len(locations)} gridpoints generated\n")
 
-    # county_locations = gps.tools.filter_grid_by_region(locations, county_gdf)
-    city_locations = gps.tools.filter_grid_by_region(locations, city_gdf)
-    # gps.tools.map_grid_locs_static(county_locations, county_gdf, "SL County Locations", f"{OUTPUT_DIR}/grid_{datetime.date.today().strftime('%Y-%m-%d')}.pdf")
-    gps.tools.map_grid_locs_static(city_locations, city_gdf, "SLC Locations", f"{OUTPUT_DIR}/grid_{datetime.date.today().strftime('%Y-%m-%d')}.pdf")
+    print("filtering to region...")
+    county_gdf = geopandas.read_file(
+        "https://slco.org/slcogis/rest/services/Administration/MapServer/3/query?outFields=*&where=1%3D1&f=geojson"
+    ).to_crs(epsg=4326)
+    # city_gdf = county_gdf.loc[county_gdf["NAME"] == "SALT LAKE"]
+
+    county_locations = gps.tools.filter_grid_by_region(locations, county_gdf)
+    print(f"\n{len(county_locations)} gridpoints within county found\n")
+    # city_locations = gps.tools.filter_grid_by_region(locations, city_gdf)
+    gps.tools.map_grid_locs_static(
+        county_locations,
+        county_gdf,
+        "SL County Locations",
+        f"{OUTPUT_DIR}/grid_{datetime.date.today().strftime('%Y-%m-%d-%H-%M')}.pdf",
+    )
+    # gps.tools.map_grid_locs_static(
+    #     city_locations,
+    #     city_gdf,
+    #     "SLC Locations",
+    #     f"{OUTPUT_DIR}/grid_{datetime.date.today().strftime('%Y-%m-%d-%H-%M')}.pdf",
+    # )
 
     print("grabbing data from Maps API...")
     results = []
-    city_locs = [f"{point.y}, {point.x}" for point in city_locations.geometry]
-    # county_locs = [f"{point.y}, {point.x}" for point in county_locations.geometry]
+    # city_locs = [f"{point.y}, {point.x}" for point in city_locations.geometry]
+    county_locs = [f"{point.y}, {point.x}" for point in county_locations.geometry]
 
     # Get nearest <=60 restaurants centered around each location
-    for location in city_locs:
+    i = 0
+    for location in county_locs:
+        i += 1
+        print(f"\nretrieving locations around gridpoint {i} of {len(county_locs)}", end="\r")
         results.extend(
             gmaps.restaurants.get_restaurants_around_location(API_KEY, LOCATION)
         )
@@ -72,6 +89,6 @@ if __name__ == "__main__":
     ]
     all_restaurants.drop_duplicates(inplace=True)
     all_restaurants.to_csv(
-        f"{OUTPUT_DIR}/locs_{datetime.date.today().strftime('%Y-%m-%d')}.csv",
+        f"{OUTPUT_DIR}/locs_{datetime.date.today().strftime('%Y-%m-%d-%H-%M')}.csv",
         index=False,
     )
